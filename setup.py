0bfd2035bd02bc1b831aa59665236918854c3d2f
und0 <andr3w.wiggin@gmail.com>
2014-03-16 18:19:19 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='twitter_me', version='1.0',
+      description='OpenShift Python-2.7 Community Cartridge based application',
+      author='und0',
+      author_email='andr3w.wiggin@gmail.com',
+      url='http://www.python.org/sigs/distutils-sig/',
+      install_requires=['pymongo' ],
+      include_package_data=True,
+     )
